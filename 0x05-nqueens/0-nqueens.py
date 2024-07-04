029e22650099baf26a9eda3a894298689412948d
RefilweM1 <133394770+RefilweM1@users.noreply.github.com>
2024-07-04 17:51:57 +0200

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,48 +1,91 @@
-var iterations = 0
+#!/usr/bin/python3
+'''N-Queens Challenge'''
 
-var print_board = function (columns) {
-  var n = columns.length, row = 0, col = 0
-  while (row < n) {
-    while (col < n) {
-      process.stdout.write(columns[row] === col ? 'Q ' : '# ')
-      col++
-    }
+import sys
 
-    process.stdout.write('\n')
-    col = 0
-    row++
-  }
-}
 
-var has_conflict = function (columns) {
-  var len = columns.length, last = columns[len - 1], previous = len - 2
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-  while (previous >= 0) {
-    if (columns[previous] === last) return true
-    if (last - (len - 1) === columns[previous] - previous) return true
-    if (last + (len - 1) === columns[previous] + previous) return true
-    previous--
-  }
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
 
-  return false
-}
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
 
-var place_next_queen = function (total, queens, columns) {
-  if (queens === 0) return columns
-  columns = columns || []
+    solutions = []
+    placed_queens = []  # coordinates format [row, column]
+    stop = False
+    r = 0
+    c = 0
 
-  for (var column = 0; column < total; column++) {
-    columns.push(column)
-    iterations++
-    if (!has_conflict(columns) &&
-        place_next_queen(total, queens - 1, columns)) {
-      return columns
-    }
-    columns.pop(column)
-  }
+    # iterate thru rows
+    while r < n:
+        goback = False
+        # iterate thru columns
+        while c < n:
+            # check is current column is safe
+            safe = True
+            for cord in placed_queens:
+                col = cord[1]
+                if(col == c or col + (r-cord[0]) == c or
+                        col - (r-cord[0]) == c):
+                    safe = False
+                    break
 
-  return null
-}
+            if not safe:
+                if c == n - 1:
+                    goback = True
+                    break
+                c += 1
+                continue
 
-print_board(place_next_queen(28, 28))
-console.log('\niterations: ', iterations)
+            # place queen
+            cords = [r, c]
+            placed_queens.append(cords)
+            # if last row, append solution and reset all to last unfinished row
+            # and last safe column in that row
+            if r == n - 1:
+                solutions.append(placed_queens[:])
+                for cord in placed_queens:
+                    if cord[1] < n - 1:
+                        r = cord[0]
+                        c = cord[1]
+                for i in range(n - r):
+                    placed_queens.pop()
+                if r == n - 1 and c == n - 1:
+                    placed_queens = []
+                    stop = True
+                r -= 1
+                c += 1
+            else:
+                c = 0
+            break
+        if stop:
+            break
+        # on fail: go back to previous row
+        # and continue from last safe column + 1
+        if goback:
+            r -= 1
+            while r >= 0:
+                c = placed_queens[r][1] + 1
+                del placed_queens[r]  # delete previous queen coordinates
+                if c < n:
+                    break
+                r -= 1
+            if r < 0:
+                break
+            continue
+        r += 1
+
+    for idx, val in enumerate(solutions):
+        if idx == len(solutions) - 1:
+            print(val, end='')
+        else:
+            print(val)
