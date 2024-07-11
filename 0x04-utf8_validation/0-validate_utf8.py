5c001a07cf08c9fd03cf4c4452fe6a5791c0872e
RefilweM1 <133394770+RefilweM1@users.noreply.github.com>
2024-07-11 20:41:44 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-"""
-0-UTF-8 Validation
-"""
+"""Challenge UTF-8"""
 
 
 def validUTF8(data):
     """
-    Data: a list of integers
-    Return: True if data is a valid UTF-8
-    encoding, else return False
+    Bit manipulation leetcode.com implementation
+    method that determines if a given data
+    set represents a valid UTF-8 encoding
+    Args:
+        data: data will be represented by a list
+              of integers
+    Return: True if data is a valid UTF-8 encoding,
+            else return False
     """
-    byte_count = 0
+
+    n_bytes = 0
+
+    m1 = 1 << 7
+    m2 = 1 << 6
 
     for i in data:
-        if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_count = 1
-            elif i >> 4 == 0b1110:
-                byte_count = 2
-            elif i >> 3 == 0b11110:
-                byte_count = 3
-            elif i >> 7 == 0b1:
+        m = 1 << 7
+        if n_bytes == 0:
+            while m & i:
+                n_bytes += 1
+                m = m >> 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            if i >> 6 != 0b10:
+            if not (i & m1 and not (i & m2)):
                 return False
-            byte_count -= 1
-    return byte_count ==
+        n_bytes -= 1
+    return n_bytes == 0
